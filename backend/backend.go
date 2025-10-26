6b996ceb03b8060c91ebfbf0ed94f84bcca4b224
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2025-10-26 18:19:03 -0400

    add: Function To Initialize Account Executive
    To allows setting private fields.

--- backend/backend.go
+++ backend/backend.go
@@ -1,8 +1,14 @@
 package backend
 
+import "github.com/kohirens/www/storage"
+
 const (
 	KeyAccountManager = "am"
 	PrefixAccounts    = "accounts"
 	PrefixLogin       = "logins"
 	PrefixGPGKey      = "secrets"
 )
+
+func NewAccountExec(store storage.Storage) *AccountExec {
+	return &AccountExec{store: store}
+}
