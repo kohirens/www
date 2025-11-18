457b95adee00ffac4d29680fc4ca2c466ed7b4ed
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2025-11-18 11:39:23 -0500

    chg: Consolidate backend Package Errors
    Merged UnauthorizeError into ReferralError. There was lots of overloap
    so this made the most sense. This should not be a breaking change since
    these have not been released.

--- backend/error.go
+++ backend/error.go
@@ -23,11 +23,12 @@ func (e *ProviderNotFound) Error() string {
 // NOTE: This is good for HTTP codes such as SeeOther, 302,301, etc. Consider
 // this a treatable error, that you choose to log or not.
 type ReferralError struct {
+	Body        []byte
 	Code        int
 	ContentType string
 	Location    string
 	Log         bool
-	msg         string
+	Message     string
 }
 
 func (e *ReferralError) Error() string {
@@ -38,7 +39,7 @@ func NewReferralError(contentType, msg, loc string, code int, log bool) *Referra
 	return &ReferralError{
 		Code:        code,
 		ContentType: contentType,
-		msg:         msg,
+		Message:     msg,
 		Location:    loc,
 		Log:         log,
 	}
@@ -50,32 +51,4 @@ type ServiceNotFoundError struct {
 
 func (e *ServiceNotFoundError) Error() string {
 	return fmt.Sprintf("service %v not found", e.name)
-}
-
-// UnauthorizedError Thrown when processing a request has failed authorization.
-// If "Location" is set, you can opt to send the client there.
-// NOTE: This is good for HTTP codes such as 401. Consider
-// this a treatable error, that you choose to log or not.
-type UnauthorizedError struct {
-	Body        []byte
-	Code        int
-	ContentType string
-	Location    string
-	Log         bool
-	msg         string
-}
-
-func (e *UnauthorizedError) Error() string {
-	return fmt.Sprintf(stderr.SeeOther, e.Location)
-}
-
-func NewUnauthorizedError(contentType, msg, loc string, code int, log bool, body []byte) *UnauthorizedError {
-	return &UnauthorizedError{
-		Body:        body,
-		ContentType: contentType,
-		Code:        code,
-		Location:    loc,
-		Log:         log,
-		msg:         msg,
-	}
 }
