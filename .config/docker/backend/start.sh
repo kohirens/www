e7c4518ba0bf8a725d7180b75c645fc2f9a9e18e
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2026-01-15 12:36:32 -0500

    chore: Added Docker Configuration
    Added a Docker configuration for development environment with AWS and
    Terraform (just because). This setup includes a backend Dockerfile,
    startup script, and a Docker Compose file to streamline the development
    process.

---
+++ .config/docker/backend/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# We'll use this script to manage starting and stopping this container gracefully.
+# It only takes up about 00.01 CPU % allotted to the container, you can verify
+# by running `docker stats` after you start a container that uses this as
+# as the CMD.
+
+set -e
+
+shutd () {
+    printf "%s" "Shutting down the container gracefully..."
+    # You can run clean commands here!
+    last_signal="15"
+}
+
+trap 'shutd' TERM
+
+go mod tidy
+
+go build -C ./ -o ~/bin/www
+
+echo "Ready!"
+
+# Run non-blocking commands here
+
+# This keeps the container running until it receives a signal to be stopped.
+# Also very low CPU usage.
+while [ "${last_signal}" != "15" ]; do
+    sleep 1
+done
+
+echo "done"
