9ef0080248bb1572c90249e2904ddb1e8a626838
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2026-04-20 22:21:30 -0400

    feat: WWW Session Reload
    Deprecated session manager load method in favor of less complicated
    reload method. It has less side effects since it does not have logic
    to set a cookie for the session.

--- session/error.go
+++ session/error.go
@@ -15,3 +15,10 @@ var StorageError = errors.New(stderr.NoStorage)
 func (e ExpiredError) Error() string {
 	return fmt.Sprintf(stderr.ExpiredCookie, e.exp.UTC().Format(time.RFC3339))
 }
+
+type NoSessionError struct {
+}
+
+func (e NoSessionError) Error() string {
+	return "no session"
+}
