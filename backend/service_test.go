61df8e45387f3c51111b644334f629b7e7910a22
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2026-04-27 22:22:39 -0400

    feat: Backend Service Manager Store and Retrieve
    These new functions wrap the service manager's add and get operations,
    allowing for type safety and better error handling when storing and
    retrieving services in the backend.

--- backend/service_test.go
+++ backend/service_test.go
@@ -1,11 +1,46 @@
 package backend
 
 import (
+	"reflect"
 	"testing"
 )
 
 type MockFixture struct {
 	Name string
+}
+
+func Test_ServiceManager_Store(t *testing.T) {
+	cases := []struct {
+		name    string
+		key     string
+		fix     *MockFixture
+		wantErr bool
+	}{
+		{
+			name:    "can-get-struct",
+			key:     "any",
+			fix:     &MockFixture{"test1234"},
+			wantErr: false,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var gotKey = NewKey[*MockFixture](c.key)
+			m := NewServiceManager()
+			Store(m, gotKey, c.fix)
+
+			got, err := Retrieve(m, gotKey)
+
+			if (err != nil) != c.wantErr {
+				t.Errorf("service() error = %v, wantErr %v", err, c.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, c.fix) {
+				t.Errorf("service() got = %v, want %v", got, c.fix)
+			}
+		})
+	}
 }
 
 func Test_service(t *testing.T) {
