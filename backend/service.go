344ab776aac1ba17f4db2a092a8ff6d78903d1e6
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2026-04-27 22:22:39 -0400

    feat: Backend Service Manager Store and Retrieve
    These new functions wrap the service manager's add and get operations,
    allowing for type safety and better error handling when storing and
    retrieving services in the backend.

--- backend/service.go
+++ backend/service.go
@@ -1,37 +1,81 @@
 package backend
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func NewServiceManager() ServiceManager {
 	return &Servicer{
-		services: make(map[string]interface{}),
+		services: make(map[string]any),
 	}
 }
 
 // ServiceManager handles storing and retrieval of dependencies when an endpoint
 // handler function is called. Granting access to objects such as Session,
-// Database, and what ever else may be useful.
+// Database, and whatever else may be useful.
 type ServiceManager interface {
-	Add(name string, service interface{})
-	Get(name string) (interface{}, error)
+	Add(name string, service any)
+	Get(name string) (any, error)
 }
 
-// Servicer A default service manager provided by the backend package.
+// Key acts as a way to provide information on the type of structure we are
+// storing. This is done because Go does NOT type parameters on methods, just
+// functions. You'll see later that we wrap methods for storing and retrieving
+// services from the service manager.
+type Key[T any] struct {
+	name string
+}
+
+// NewKey associates each key in the service manager list with the type that it
+// stores.
+func NewKey[T any](name string) Key[T] {
+	return Key[T]{name}
+}
+
+// Servicer the default service manager provided by the backend package.
 type Servicer struct {
-	services map[string]interface{}
+	services map[string]any
 }
 
-// Add adds a service to a map for access from an endpoint function.
-func (m *Servicer) Add(name string, service interface{}) {
+// Add adds a service to the list of services.
+//
+//	NOTE: This is for internal use only, it may be made private in the future.
+//	Please use the Store[T any]() function.
+func (m *Servicer) Add(name string, service any) {
 	m.services[name] = service
 }
 
-// Get Retrieves a service from.
-func (m *Servicer) Get(name string) (interface{}, error) {
+// Get Retrieves a service from the list of services.
+//
+//	NOTE: This is for internal use only, it may be made private in the future.
+//	Please use the Retrieve[T any]() function.
+func (m *Servicer) Get(name string) (any, error) {
 	x, ok := m.services[name]
 	if !ok {
 		return nil, fmt.Errorf(stderr.ServiceNotFound, name)
 	}
 
 	return x, nil
+}
+
+// Store adds a service to a map for access from an endpoint function.
+func Store[T any](m ServiceManager, key Key[T], service T) {
+	m.Add(key.name, service)
+}
+
+// Retrieve Retrieves a service from the list of services.
+func Retrieve[T any](m ServiceManager, key Key[T]) (T, error) {
+	stored, e1 := m.Get(key.name)
+	if e1 != nil {
+		var nilVar T
+		return nilVar, e1
+	}
+
+	service, ok := stored.(T)
+	if !ok {
+		var nilVar T
+		return nilVar, fmt.Errorf("%v", stderr.ServicePointer)
+	}
+
+	return service, nil
 }
